23bd255590bf6a41bd4a390b917e2fef85c076fe
Andrew Rodriguez <andrewrb22@gmail.com>
2022-08-23 19:02:27 -0400

    First commit , print statement

---
+++ Day1Notes.java
@@ -0,0 +1,18 @@
+//Mandatory comments for each assigments
+
+//Andrew Rodriguez
+//Day 1 Notes
+//Aug 23, 2022
+//COP3330
+
+public class Day1Notes {
+
+    public static void main(String[] args){
+
+        //Print Statements
+System.out.print("Hello world");
+
+
+    }
+    
+}
