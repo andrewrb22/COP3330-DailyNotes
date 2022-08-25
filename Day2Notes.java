f76434c4c3234a6f0b26c94ae5794617c5339eb4
Andrew Rodriguez <andrewrb22@gmail.com>
2022-08-25 18:25:58 -0400

    scan.next** the ** is the type of variable , example int, double, boolean ,etc

--- Day2Notes.java
+++ Day2Notes.java
@@ -1,25 +1,22 @@
 import java.util.Scanner;
 
 public class Day2Notes {
-    
- public static void main(String[] args) {
-    
-    //WORKING ON INPUTS
-    //Create a scanner so we can accept text input;
-    int happy;
-    Scanner scan = new Scanner(System.in);
-System.out.println("How many pizza do you need?");
-    int pizza = scan.nextInt();
-    System.out.print("\nthere are " + (pizza * 8) + " Slices in your order");
-    System.out.print("\nthere are " + pizza + 8 + " Slices in your order");
 
+    public static void main(String[] args) {
 
+        // WORKING ON INPUTS
+        // Create a scanner so we can accept text input;
+        int happy;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How much is a pizza?");
+       // int pizza = scan.nextInt();
+        double price = scan.nextDouble();
+       // System.out.print("\nthere are " + (pizza * 8) + " Slices in your order");
+        System.out.print("\nthere are " + price  + " Slices in your order");
 
-    //Closes the scanner
-    scan.close();
+        // Closes the scanner
+        scan.close();
 
-
-}
-
+    }
 
 }
